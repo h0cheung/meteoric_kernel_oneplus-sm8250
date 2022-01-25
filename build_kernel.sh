4e367dea5690ed416bcc729d62039f0e6ec442d7
Howard Cheung <mail@h-cheung.cf>
2022-01-25 19:59:44 +0800

    Update build script for my PC.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE="/home/fra/gcc-12/bin/aarch64-linux-gnu-"
+export CROSS_COMPILE="/HDD/android_kernel_build_tools/gcc-12/bin/aarch64-linux-gnu-"
+export PATH="/HDD/android_kernel_build_tools/clang/bin:$PATH"
 
-make clean -j69 && make mrproper -j69
+make clean -j8 && make mrproper -j8
 
-PATH="/home/fra/clang/bin:${PATH}" \
-	make O=out HOSTCC="ccache clang" CC="ccache clang" HOSTCXX="ccache clang++" HOSTLD="ccache ld.lld" LD="ccache ld.lld" HOSTAR="ccache llvm-ar" AR="ccache llvm-ar" NM="ccache llvm-nm" STRIP="ccache llvm-strip" vendor/kona-perf_defconfig -j69
+make O=out HOSTCC="ccache clang" CC="ccache clang" HOSTCXX="ccache clang++" HOSTLD="ccache ld.lld" LD="ccache ld.lld" HOSTAR="ccache llvm-ar" AR="ccache llvm-ar" NM="ccache llvm-nm" OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf OBJSIZE=llvm-size STRIP="ccache llvm-strip" vendor/kona-perf_defconfig -j8
 
-PATH="/home/fra/clang/bin:${PATH}" \
-	make O=out HOSTCC="ccache clang" CC="ccache clang" HOSTCXX="ccache clang++" HOSTLD="ccache ld.lld" LD="ccache ld.lld" HOSTAR="ccache llvm-ar" AR="ccache llvm-ar" NM="ccache llvm-nm" STRIP="ccache llvm-strip" -j69
+make O=out HOSTCC="ccache clang" CC="ccache clang" HOSTCXX="ccache clang++" HOSTLD="ccache ld.lld" LD="ccache ld.lld" HOSTAR="ccache llvm-ar" AR="ccache llvm-ar" NM="ccache llvm-nm" OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf OBJSIZE=llvm-size STRIP="ccache llvm-strip" -j8
 
